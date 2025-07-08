4ce82b7c425d9622d27bbade0fbfc7885e25b27a
Muskan Agarwal <muskanagarwal3110@gmail.com>
2025-07-09 02:35:47 +0530

    modified

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,9 +27,9 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV=== "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist" )))
 
-    app.get("/{*}", (req, res)=>{
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html" ))
-    })
+    app.get("/:wildcard(*)", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 }
 server.listen(PORT, ()=>{
     console.log("server running on PORT:"+ PORT)
